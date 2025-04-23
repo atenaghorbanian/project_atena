7ed6bd9e0445f3e8190e5fec0c1ee0fdc50f68c8
maryam <maryammollaei8181@gmail.com>
2025-04-24 00:02:46 +0330

    create function populatin for create list chromosom random

--- scode.py
+++ scode.py
@@ -7,3 +7,12 @@ GENERATIONS = 100
 def fitness(chromosome):
     # هر چه تعداد بیت های 1 بیشتر باشد، fitness بهتر است
     return sum(chromosome)
+
+
+# ایجاد جمعیت اولیه
+def create_population(size, gene_length):
+    population = []
+    for _ in range(size):
+        chromosome = [random.randint(0, 1) for _ in range(gene_length)]
+        population.append(chromosome)
+    return population
